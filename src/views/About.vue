8e6e45fd5e8f2039af187535f776f67013ee5b11
Salman Javed <salmanjaved5050@gmail.com>
2025-05-12 22:20:05 +0500

    Update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,9 +4,9 @@
 
     <div class="paragraph">
       <div>
-        I’m <strong>Salman Javed</strong>—a gameplay programmer with 8 years of experience crafting engaging games and interactive experiences.<br/><br/>
-        As both a developer and an avid gamer, I thrive on dissecting game design, mechanics, and systems—always eager to brainstorm innovative ideas or discuss what makes games truly compelling.<br/><br/>
-        My expertise spans Roblox and Unity3D, where I’ve built games that balance creativity with technical execution. Let’s talk games, design and player experiences!
+        I'm <strong>Salman Javed</strong>—a gameplay programmer with 8 years of experience crafting engaging games and interactive experiences.<br/><br/>
+        As both a developer and an avid gamer, I love dissecting game design, mechanics, and systems. Always eager to brainstorm innovative ideas or discuss what makes games truly compelling.<br/><br/>
+        My expertise spans Roblox and Unity3D, where I've built games that balance creativity with technical execution. Let's talk games, design and player experiences!
       </div>
 
 <!-- <div style="margin-top: 20px;">Some of the <router-link to="/game-projects">roblox game projects</router-link> I've worked on, along with <router-link to="/other-projects">other stuff</router-link>.</div> -->
